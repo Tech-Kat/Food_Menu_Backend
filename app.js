227f63201a4bb18b7f6ce9a9897d8d487a24beff
Tech-Kat <katpelle@pursuit.org>
2023-07-29 17:17:54 -0400

    api retrieved and routes made

--- app.js
+++ app.js
@@ -24,6 +24,22 @@ res.status(500).json({error: err.message})
 
 //('/items/:id') Path
 
+app.get("/items/:id", (req, res) => {
+    try {
+      const { id } = req.params;
+      const items  = itemsData;
+  
+      const item = items.find((el) => el.id === id);
+      if (item) {
+        return res.status(200).json({ data: item });
+      }
+      res.status(404).json({ error: `Could not find item with this ${id}`});
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+
+  });
+
 
 
 module.exports = app;
